90e375358f24618de9f59f8b6671f985d5bf96ab
VincentB <vincent.brunelle.gm@gmail.com>
2025-09-25 19:48:36 -0400

    ADD : Function to print a row

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Game
+{
+    public void PrintBig(int[] arr)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            foreach (int digit in arr)
+            {
+                Console.Write(StringDigits.DigitLayers[i][digit]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Game game = new Game();
+        game.PrintBig([1,5,4,3,6,7,2,3,4,5]);
+    }
+}
